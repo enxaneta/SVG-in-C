694d2b993ebe2388fc7c5aeb4efe9b49350a2f96
enxaneta <gabrielasolersiques@yahoo.com>
2019-04-22 19:18:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let the_d, closed, lc_d_ry, uc_d_ry;
+let the_d, closed, lc_d_ry, uc_d_ry, C_d_ry;
 
 Init();
 
@@ -9,20 +9,23 @@ TA.addEventListener("input", () => {
 function Init() {
   // the value of the d is the value of the textarea
   the_d = TA.value;
-  // set the d attribute of the path
+  // set the d atteibute of the path
   thePath.setAttributeNS(null, "d", the_d);
   // set the viewBox of the svg element
   setViewBox(thePath);
   // is the path closed?
   closed = null;
   //formatedArgsArray takes care of long commands
+  
   lc_d_ry = formatedArgsArray(the_d);
   //SVG2UpperCase changes all commands to uppercase
   uc_d_ry = SVG2UpperCase(lc_d_ry);
 
-  let UC_path = joinRy2d(uc_d_ry);
-  UCPath.setAttributeNS(null, "d", UC_path);
-  output.textContent = UC_path;
+  let C_d_ry = SVG2C(uc_d_ry);
+
+  let C_path = joinRy2d(C_d_ry);
+  CPath.setAttributeNS(null, "d", C_path);
+  output.textContent = C_path;
 }
 
 function setViewBox(thePath) {
@@ -153,14 +156,11 @@ function SVG2UpperCase(commands) {
 
       switch (c[0]) {
         case "v":
-          console.log("v",c[1],y_)
           c[0] = "L";
-          c[2] = Number(c[1]) + y_; //y
           c[1] = x_; //x
-          
+          c[2] = Number(c[2]) + y_; //y
           break;
         case "h":
-          console.log("h",c[1])
           c[0] = "L";
           c[1] = Number(c[1]) + x_; //x
           c[2] = y_; //y
@@ -194,6 +194,121 @@ function SVG2UpperCase(commands) {
   }
 
   return commands;
+}
+
+//-function and helpers to change Upper Case commands to C
+
+// THERE IS NO FUNCTION TO CHANGE A TO C :(
+
+function SVG2C(UC_d_ry) {
+  let C_d_ry = [UC_d_ry[0]];
+  for (let i = 1; i < UC_d_ry.length; i++) {
+    let c = UC_d_ry[i];
+    let prev = UC_d_ry[i - 1];
+
+    switch (c[0]) {
+      case "S":
+        C_d_ry.push(S2C(prev, c));
+        break;
+      case "Q":
+        C_d_ry.push(T2C(prev, c));
+        break;
+      case "T":
+        C_d_ry.push(T2C(prev, c));
+        break;
+      case "L":
+        C_d_ry.push(L2C(prev, c));
+        break;
+      default:
+        C_d_ry.push(c);
+    }
+  }
+
+  return C_d_ry;
+}
+// a function to change Q to C
+function Q2C(previous, command) {
+  for (let i = 1; i < previous.length; i++) {
+    previous[i] = parseFloat(previous[i]);
+  }
+  for (let i = 1; i < command.length; i++) {
+    command[i] = parseFloat(command[i]);
+  }
+
+  let Q = [
+    [previous[previous.length - 2], previous[previous.length - 1]],
+    command
+  ];
+
+  let cp1 = {};
+  let cp2 = {};
+  cp1.x = Number(Q[0][0] + 2 / 3 * (Q[1][1] - Q[0][0]));
+  cp1.y = Number(Q[0][1] + 2 / 3 * (Q[1][2] - Q[0][1]));
+  cp2.x = Number(Q[1][3] + 2 / 3 * (Q[1][1] - Q[1][3]));
+  cp2.y = Number(Q[1][4] + 2 / 3 * (Q[1][2] - Q[1][4]));
+
+  return [
+    "C",
+    cp1.x,
+    cp1.y,
+    cp2.x,
+    cp2.y,
+    Number(command[3]),
+    Number(command[4])
+  ];
+}
+// a function to change L to C
+function L2C(previous, command) {
+  //line to cubic Bézier
+  let x0 = Number(previous[previous.length - 2]);
+  let y0 = Number(previous[previous.length - 1]);
+  let x3 = Number(command[1]);
+  let y3 = Number(command[2]);
+  let x1 = Number(x0 + (x3 - x0) / 3);
+  let y1 = Number(y0 + (y3 - y0) / 3);
+  let x2 = Number(x0 + 2 * (x3 - x0) / 3);
+  let y2 = Number(y0 + 2 * (y3 - y0) / 3);
+
+  return ["C", x1, y1, x2, y2, x3, y3];
+} //helper to change a L to C
+//a function to change the T command to a Q command
+function T2Q(previous, command) {
+  let reflected = getReflectedPoint(previous, command);
+  return ["Q", reflected.x, reflected.y, command[1], command[2]];
+}
+//a function to change the T command to a C command
+function T2C(previous, command) {
+  let Q = T2Q(previous, command);
+  return Q2C(previous, command);
+}
+//a function to change the S command to a C command
+function S2C(previous, command) {
+  let reflected = getReflectedPoint(previous, command);
+  return [
+    "C",
+    reflected.x,
+    reflected.y,
+    command[1],
+    command[2],
+    command[3],
+    command[4]
+  ];
+}
+// a function to get the reflected point for the T & S commands
+function getReflectedPoint(previous, command) {
+  let x2 = Number(previous[previous.length - 4]); //cp x
+  let y2 = Number(previous[previous.length - 3]); //cp y
+  let x3 = Number(previous[previous.length - 2]); //joining point x
+  let y3 = Number(previous[previous.length - 1]); //joining point y
+
+  ///////////////////////////////////////
+  // the reflected control point
+  let reflected = {};
+  reflected.x = 2 * x3 - x2;
+  reflected.y = 2 * y3 - y2;
+  ///////////////////////////////////////
+
+  return reflected;
 }
 
 function joinRy2d(argsRy) {
